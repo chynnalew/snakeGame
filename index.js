bd3572f47bbc509e7e3e266f1f1f6918860861a0
chynnalew <chynnalew@yahoo.com>
2021-10-06 12:26:08 -0700

    install express, add index.js, add.gitignore

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+//express framework for node.js 
+//$npm install express --save
+//$ node index.js (to run the code in the  browser)
