cf6b92641a69e499c63d607c8a1ea1b39d0d0bd6
chynnalew <chynnalew@yahoo.com>
2021-10-06 12:50:48 -0700

    update express index.js, add phaser.min to lib, add snakeGame.js main game file

--- index.js
+++ index.js
@@ -2,13 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html')
-})
+app.use(express.static(__dirname + '/src/'));
+app.get('/', (req, res) => res.sendFile('index.html'));
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
 
 //express framework for node.js 
 //$npm install express --save
